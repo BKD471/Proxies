ac3fd862bbf6af240d5d8724b2d6bf01ba191463
BKD471 <bhaskarkumardas9@gmail.com>
2024-03-02 00:19:15 +0530

    setting up proxy

--- src/main/java/LeetCodeDao.java
+++ src/main/java/LeetCodeDao.java
@@ -1,40 +1,39 @@
-import java.util.EnumSet;
 import java.util.List;
 
-public class LeetCodeDao implements ILeetCodeDashBoard{
+public class LeetCodeDao implements ILeetCodeDashBoard {
     /**
-     * @return 
+     * @return
      */
     @Override
     public List<QuestionsData> showQuestionsList(final AuthObj authObj) {
         // db calls to fetch questions
-        List<QuestionsData> fetchedQuestions= List.of(new QuestionsData("1","Two Sum",true,List.of(Tags.HASHING,Tags.SORTING)),
-                new QuestionsData("2","Three Sum",true,List.of(Tags.HASHING,Tags.SORTING,Tags.TWO_POINTERS)));
+        List<QuestionsData> fetchedQuestions = List.of(new QuestionsData("1", "Two Sum", true, List.of(Tags.HASHING, Tags.SORTING)),
+                new QuestionsData("2", "Three Sum", true, List.of(Tags.HASHING, Tags.SORTING, Tags.TWO_POINTERS)));
         return fetchedQuestions;
     }
 
     /**
-     * @param tag 
+     * @param tag
      * @return
      */
     @Override
-    public List<QuestionsData> filterByTags(final AuthObj authObj,final String tag) {
+    public List<QuestionsData> filterByTags(final AuthObj authObj, final String tag) {
         // db calls to fetch questions
-        List<QuestionsData> fetchedQuestions= List.of(new QuestionsData("1","Two Sum",true,List.of(Tags.HASHING,Tags.SORTING)),
-                new QuestionsData("2","Three Sum",true,List.of(Tags.HASHING,Tags.SORTING,Tags.TWO_POINTERS)));
-       List<QuestionsData> filteredQuestions=fetchedQuestions.stream()
-               .filter(question-> question.tags().contains(Tags.valueOf(tag))).toList();
+        List<QuestionsData> fetchedQuestions = List.of(new QuestionsData("1", "Two Sum", true, List.of(Tags.HASHING, Tags.SORTING)),
+                new QuestionsData("2", "Three Sum", true, List.of(Tags.HASHING, Tags.SORTING, Tags.TWO_POINTERS)));
+        List<QuestionsData> filteredQuestions = fetchedQuestions.stream()
+                .filter(question -> question.tags().contains(Tags.valueOf(tag))).toList();
         return filteredQuestions;
     }
 
     /**
-     * @param qId 
+     * @param qId
      * @return
      */
     @Override
     public Question showQuestion(final String qId) {
         // fetch question from db
-        Question questions=new Question();
+        Question questions = new Question();
         return questions;
     }
 }
